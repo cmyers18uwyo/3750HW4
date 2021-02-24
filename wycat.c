693630c498a155dd455d7179e143a7a8f0ee1536
Charles Myers <55416416+cmyers18uwyo@users.noreply.github.com>
2021-02-23 19:47:38 -0700

    Add files via upload

--- wycat.c
+++ wycat.c
@@ -10,25 +10,33 @@
 
 #include <stdio.h>
 #include <errno.h>
-#include <string. h>
+#include <string.h>
 int main(int argc, char* argv[]) {
-//Check to see if we have any arguments
+//Declare vars for our file and buffer
 FILE *infile;
 char buffer[4096];
+//Check to see if we have any arguments
 	if(argc > 1) {
+		//iterate through our arguments
 		for(int i = 1; argv[i] != NULL; i++) {
+			//If an argument is - then copy the users standard input
+			//to standard output
 			if(strcmp(argv[i], "-") == 0) {
-				
+				while(fread(buffer, sizeof(char), 64, stdin) == 64) {
+						fwrite(buffer,sizeof(char), 64, stdout);
+					}
 			}
 			else {
+				//Try to open the file from the arguments
 				infile=fopen(argv[i], "r");
+				//If its not a file return an error, but dont stop the program
 				if(infile == NULL) {
 					perror(argv[i]);
 				}
 				else {
-					
-					while(fread(buffer, sizeof(char), 4, infile) == 4) {
-						printf("%s\n", buffer);
+					//Read the data from the file then copy it to standard output
+					while(fread(buffer, sizeof(char), 64, infile) == 64) {
+						fwrite(buffer,sizeof(char), 64, stdout);
 					}
 					fclose(infile);
 				}
@@ -37,5 +45,10 @@ char buffer[4096];
 		}
 	
 	}
-
+	else {
+		while(fread(buffer, sizeof(char), 64, stdin) == 64) {
+			fwrite(buffer,sizeof(char), 64, stdout);
+		}
+		
+	}
 }
